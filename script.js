46385b0ed2c0874be708e765540c0e497ad27990
microwave on a fridge <108324217+microwave-on-a-fridge@users.noreply.github.com>
2023-04-05 15:19:24 +1200

    work has been done

--- script.js
+++ script.js
@@ -6,32 +6,44 @@
 **/
 
 const WIDTH = 600;
-const HEIGHT = 500;
-
+const HEIGHT = 800;
+const RECWIDTH = WIDTH
+const RECHEIGHT = 50;
+var recXPosition = 0;
+var recYPosition = 650;
 var ctx;
-
 window.onload = startCanvas;
 
 function startCanvas() {
-    // The startCanvas() function sets up the game.
-    // This is where all of the once off startup stuff should go
 
     ctx = document.getElementById("myCanvas").getContext("2d");
-    // This timer sets the framerate.
-    // 10 means 10 milliseconds between frames (100 frames per second)
     timer = setInterval(updateCanvas, 20);
 }
 
 function updateCanvas() {
-    // The updateCanvas() function contains the main game loop
-    // It is run once every frame. Most of the game code will go here
+  ctx.fillStyle = "#EFB0FF";
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  ctx.fillStyle = "#7FFFD4";
+  ctx.fillRect(
+    recXPosition,
+    recYPosition,
+    RECWIDTH,
+    RECHEIGHT
+  );
 
-    // Clear the frame
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, WIDTH, HEIGHT);
-    // Task 2b
-    // Draw the background
-    // The background image is already the correct size, it just needs to be positioned in the top left
-    // Uncomment the line below and fix the coordinates
-    ctx.drawImage(backgroundImage, 0, 0);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(playerXPosition, playerYPosition, PLAYERWIDTH, PLAYERHEIGHT);
+}
+
+function collisionDetected() {
+  if (
+    playerXPosition + PLAYERWIDTH >= obstacleXPosition &&
+    playerXPosition <= obstacleXPosition + OBSTACLEWIDTH &&
+    playerYPosition + PLAYERHEIGHT >= obstacleYPosition &&
+    playerYPosition <= obstacleYPosition + OBSTACLEHEIGHT
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
