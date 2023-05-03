1535bc70790212b22d3009bf5e0a74f149fc2d53
microwave on a fridge <108324217+microwave-on-a-fridge@users.noreply.github.com>
2023-05-04 09:37:06 +1200

    4/5

--- index.js
+++ index.js
@@ -21,38 +21,51 @@ const ENEMY_HEIGHT = 38;
 const ENEMY_X_SPEED = 6;
 const ENEMY_Y_SPEED = 6;
 const ENEMY_COLOR = "#FF2222";
-/*
-const ENEMY_X_SPAWNING = [0, WIDTH];
-const ENEMY_X_SPAWNING_RNG =
-  ENEMY_X_SPAWNING[Math.floor(Math.random() * ENEMY_X_SPAWNING.length)];
-const ENEMY_Y_SPAWNING = [0, HEIGHT];
-const ENEMY_Y_SPAWNING_RNG =
-  ENEMY_Y_SPAWNING[Math.floor(Math.random() * ENEMY_Y_SPAWNING.length)];
-*/
+const HARD_ENEMY_WIDTH = 50;
+const HARD_ENEMY_HEIGHT = 50;
+const HARD_ENEMY_X_SPEED = 3;
+const HARD_ENEMY_Y_SPEED = 5;
 
 var ctx;
-var level = 1;
+var level = 0;
 var playerXPosition = 30;
 var playerYPosition = 30;
 var playerColor;
 var playerImage = new Image();
 playerImage.src = "images/marisa.png";
 var enemyArray = [];
-var enemyCap = 20;
+var enemyCap = 10;
 var enemyImage = new Image();
 enemyImage.src = "images/cirno.png";
+var hardEnemyArray = [];
+var hardEnemyCap = 3;
+var hardEnemyImage = new Image();
+hardEnemyImage.src = "images/hard.png";
 
 window.onload = startCanvas;
 
 function startCanvas() {
   ctx = document.getElementById("myCanvas").getContext("2d");
   timer = setInterval(updateCanvas, 20);
+  timer = setInterval(progression, 5000);
+}
 
+function progression() {
   var enemyNumber = 0;
   while (enemyNumber < enemyCap) {
     enemyArray.push(new Enemy(Math.random() * WIDTH));
     enemyNumber++;
   }
+  if (level > 20) {
+    var hardEnemyNumber;
+    while (hardEnemyNumber < hardEnemyCap) {
+      hardEnemyArray.push(new HardEnemy(Math.random() * WIDTH));
+      hardEnemyNumber++;
+    }
+  }
+  level++;
+  enemyCap + 5;
+  hardEnemyCap + 1;
   console.log("There are", enemyArray.length, "enemies in the enemyArray");
 }
 
@@ -71,39 +84,40 @@ function updateCanvas() {
     enemyNumber++;
   }
 
+  var hardEnemyNumber = 0;
+  while (hardEnemyNumber < hardEnemyArray.length) {
+    hardEnemyArray[hardEnemyNumber].moveHardEnemy();
+    hardEnemyNumber++;
+  }
+
   playerColor = PLAYER_COLOR;
 
-  var enemyNumber = 0;
-  while (enemyNumber < enemyArray.length) {
+  var hardEnemyNumber = 0;
+  while (hardEnemyNumber < hardEnemyArray.length) {
     if (
       playerHit(
-        enemyArray[enemyNumber].xPosition,
-        enemyArray[enemyNumber].yPosition
+        hardEnemyArray[hardEnemyNumber].xPosition,
+        hardEnemyArray[hardEnemyNumber].yPosition
       )
     ) {
       playerColor = PLAYER_HIT_COLOR;
-      enemyArray[enemyNumber].yPosition = Math.random() * -HEIGHT;
+      hardEnemyArray[hardEnemyNumber].yPosition = Math.random() * -HEIGHT;
     }
-    enemyNumber++;
+    hardEnemyNumber++;
   }
 
-  var enemyNumber = 0;
-  while (enemyNumber < enemyArray.length) {
+  var hardEnemyNumber = 0;
+  while (hardEnemyNumber < hardEnemyArray.length) {
     //ctx.fillStyle = ENEMY_COLOR;
     ctx.drawImage(
-      enemyImage,
-      enemyArray[enemyNumber].xPosition,
-      enemyArray[enemyNumber].yPosition,
-      ENEMY_WIDTH,
-      ENEMY_HEIGHT
+      hardEnemyImage,
+      hardEnemyArray[hardEnemyNumber].xPosition,
+      hardEnemyArray[hardEnemyNumber].yPosition,
+      HARD_ENEMY_WIDTH,
+      HARD_ENEMY_HEIGHT
     );
-    enemyNumber++;
+    hardEnemyNumber++;
   }
-
-  /*
-  ctx.fillStyle = playerColor;
-  ctx.fillRect(playerXPosition, playerYPosition, PLAYER_WIDTH, PLAYER_HEIGHT);
-  */
 
   ctx.drawImage(
     playerImage,
@@ -115,33 +129,32 @@ function updateCanvas() {
 }
 
 class Enemy {
-  /// Note that the name of the class starts with a capital letter, like the Math object. This is a convention that makes it easier to read the code.
-  /* 
-	The Rain class contains everything to do with rain. This includes:
-		stuff about the rain - it's variables (or properties)
-		and what the rain does - it's functions (or methods)
-	*/
   constructor(x) {
-    /* 
-		All classes MUST have a constructor.
-		The constructor sets up the properties. Each one starts with "this." That tells JavaScript that it is a variable inside this object.
-		All variables to do with this object that are not CONST need to be defined here.
-		*/
     this.xPosition = Math.random() * -WIDTH;
-    // Task 8a
-    // The rain looks terrible!
-    // The drops should start at random heights
-    // Change the line below that sets the yPosition
-    // Make them appear at a random position above the canvas
-    // A y position between 0 and -HEIGHT (negative y is above the canvas)
     this.yPosition = Math.random() * -HEIGHT;
   }
   moveEnemy() {
-    // Move the rain - Change it's position
     this.xPosition += ENEMY_X_SPEED;
     this.yPosition += ENEMY_Y_SPEED;
 
-    // When rain it's the bottom, restart at the top
+    if (this.xPosition > WIDTH) {
+      this.xPosition = 0;
+    }
+    if (this.yPosition > HEIGHT) {
+      this.yPosition = 0;
+    }
+  }
+}
+
+class HardEnemy {
+  constructor(x) {
+    this.xPosition = Math.random() * -WIDTH;
+    this.yPosition = Math.random() * -HEIGHT;
+  }
+  moveHardEnemy() {
+    this.xPosition += ENEMY_X_SPEED;
+    this.yPosition += ENEMY_Y_SPEED;
+
     if (this.xPosition > WIDTH) {
       this.xPosition = 0;
     }
@@ -168,6 +181,7 @@ function playerHit(enemyX, enemyY) {
   var enemyHitHeight = enemyHitBottom - enemyHitTop;
 
   /*
+  //DEBUG DEBUG DEBUG
   var keyDown = keyboardEvent.key;
   if (keyDown == "a") {
     console.log("Debug mode enabled!");
@@ -204,9 +218,4 @@ window.addEventListener("mousemove", mouseMovedFunction);
 function mouseMovedFunction(mouseEvent) {
   playerXPosition = mouseEvent.offsetX;
   playerYPosition = mouseEvent.offsetY;
-}
-
-setInterval(progression, 2000);
-function progression() {
-  level++;
 }
